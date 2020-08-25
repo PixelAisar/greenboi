5e7d7daa7d2dd3927235b52dde82b40377df72f5
PixelAisar <57201250+PixelAisar@users.noreply.github.com>
2020-08-25 04:32:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,43 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
+const { Client, RichEmbed } = require("discord.js");
+var { Util } = require("discord.js");
+const { TOKEN, YT_API_KEY, prefix, devs } = require("./config");
+const client = new Client({ disableEveryone: true });
+const ytdl = require("ytdl-core");
+const canvas = require("canvas");
+const Canvas = require("canvas");
+const convert = require("hh-mm-ss");
+const fetchVideoInfo = require("youtube-info");
+const botversion = require("./package.json").version;
+const simpleytapi = require("simple-youtube-api");
+const moment = require("moment");
+const fs = require("fs");
+const util = require("util");
+const gif = require("gif-search");
+const opus = require("node-opus");
+const ms = require("ms");
+const jimp = require("jimp");
+const { get } = require("snekfetch");
+const guild = require("guild");
+const dateFormat = require("dateformat"); //npm i dateformat
+const YouTube = require("simple-youtube-api");
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const hastebins = require("hastebin-gen");
+const getYoutubeID = require("get-youtube-id");
+const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
+const pretty = require("pretty-ms");
+client.login(TOKEN);
+const queue = new Map();
+var table = require("table").table;
+const Discord = require("discord.js");
+const prefix = '.'
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setStatus("dnd")
 });
+
+const dev = ["604758234057670686","635186941977165824"];
+const admin = "=";
+var owner = "604758234057670686","635186941977165824"; 
 
 client.on('message', message => {
     var args = message.content.split(/[ ]+/)
@@ -15,9 +48,42 @@ client.on('message', message => {
     }
 });
 
+client.on("message", message => {
+  if (message.author.bot) return;
+  if (message.content === prefix + "help") {
+    message.channel.send(
+      `**:smiling_imp:  | The commands has been sent in your dms. **`
+    );
+
+    message.author.sendMessage(` ✽ **__ Depex Bot v1__**
+**__General Commands__** 
+**  .bot • Shows information about the bot.** 
+**  .user • Shows information about you.** 
+**  .avt • Shows your avatar or anyone avatar with linking the id.** 
+**  .avatar • Shows your avatar or the one who you mentioned.** 
+**  .color • Choosing a color role in the server **
+**__Administrator Commands__**
+**  .clear • Clears the chat.** 
+**  .ban • Bans someone you mentioned.** 
+**  .kick • Kicks someone you mentioned** 
+**  .mute • Mutes someone you mentioned.** 
+**  .unmute • Unmutes someone you mentioned.** 
+**  .move •  Moves someone to your voice call.**
+**  .giveaway •   Makes a giveaway about something you want to give.**
+
+`);
+  }
+});
+
 client.on('message', msg => {
   if (msg.content === '.ping') {
     msg.reply('Pong!');
+  }
+});
+
+client.on("message", msg => {
+  if (msg.content === "السلام عليكم") {
+    msg.reply("** وعليكم السلام ورحمة الله وبركاته 💖💕**  ");
   }
 });
 
@@ -54,6 +120,224 @@ ${args}`);
       });
   }
 
+});
+
+client.on("message", async message => {
+  const moment = require("moment"); //npm i moment
+  const ms = require("ms"); //npm i ms
+  // var prefix = '' //Bot Prefix !
+  var time = moment().format("Do MMMM YYYY , hh:mm");
+  var room;
+  var title;
+  var duration;
+  var currentTime = new Date(),
+    hours = currentTime.getHours() + 3,
+    minutes = currentTime.getMinutes(),
+    done = currentTime.getMinutes() + duration,
+    seconds = currentTime.getSeconds();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  var suffix = "AM";
+  if (hours >= 12) {
+    suffix = "PM";
+    hours = hours - 12;
+  }
+  if (hours == 0) {
+    hours = 12;
+  }
+
+  var filter = m => m.author.id === message.author.id;
+  if (message.content.startsWith(prefix + "gcreate")) {
+    let embed1 = new Discord.RichEmbed()
+      .setColor()
+      .setDescription("Missing the following permission `MANAGE_GUILD`");
+
+    let embed2 = new Discord.RichEmbed()
+      .setColor()
+      .setDescription("Please send the `room` name without mentioning it");
+
+    let embed3 = new Discord.RichEmbed()
+      .setColor()
+      .setDescription("Wrong room name");
+
+    let embed4 = new Discord.RichEmbed()
+      .setColor()
+      .setDescription("Please send the `time`");
+
+    let embed5 = new Discord.RichEmbed()
+      .setColor()
+      .setDescription(
+        "Wrong time format\nExample of time format: 1s / 1m / 1h / 1d / 1w"
+      );
+
+    let embed6 = new Discord.RichEmbed()
+      .setColor()
+      .setDescription("Please send the `gift`");
+
+    if (!message.guild.member(message.author).hasPermission("MANAGE_GUILD"))
+      return message.channel.send(embed1);
+    message.channel.send(embed2).then(msg => {
+      message.channel
+        .awaitMessages(filter, {
+          max: 1,
+          time: 20000,
+          errors: ["time"]
+        })
+        .then(collected => {
+          let room = message.guild.channels.find(
+            "name",
+            collected.first().content
+          );
+          if (!room) return message.channel.send(embed3);
+          room = collected.first().content;
+          collected.first().delete();
+          msg.edit(embed4).then(msg => {
+            message.channel
+              .awaitMessages(filter, {
+                max: 1,
+                time: 20000,
+                errors: ["time"]
+              })
+              .then(collected => {
+                if (!collected.first().content.match(/[1-60][s,m,h,d,w]/g))
+                  return message.channel.send(embed5);
+                duration = collected.first().content;
+                collected.first().delete();
+                msg.edit(embed6).then(msg => {
+                  message.channel
+                    .awaitMessages(filter, {
+                      max: 1,
+                      time: 20000,
+                      errors: ["time"]
+                    })
+                    .then(collected => {
+                      title = collected.first().content;
+                      collected.first().delete();
+                      msg.delete();
+                      message.delete();
+                      try {
+                        let giveEmbed = new Discord.RichEmbed()
+                          .setColor()
+                          .setTitle(`${title}`)
+                          .setDescription(
+                            `React With 🎉 To Enter! \nTime remaining : ${duration} \n **Created at :** ${hours}:${minutes}:${seconds} ${suffix}`
+                          );
+                        //.setFooter(message.author.username, message.author.avatarURL);
+                        message.guild.channels
+                          .find("name", room)
+                          .send(" :tada: **Giveaway** :tada:", {
+                            embed: giveEmbed
+                          })
+                          .then(m => {
+                            let re = m.react("🎉");
+                            setTimeout(() => {
+                              let users = m.reactions.get("🎉").users;
+                              let list = users
+                                .array()
+                                .filter(
+                                  u => (u.id !== m.author.id) !== client.user.id
+                                );
+                              let gFilter =
+                                list[
+                                  Math.floor(Math.random() * list.length) + 1
+                                ];
+                              if (gFilter === undefined) {
+                                let endEmbed = new Discord.RichEmbed()
+                                  .setColor()
+                                  .setTitle(title)
+                                  .setDescription(
+                                    `Winners : no enough number of reaction so there is no winner`
+                                  )
+                                  .setFooter("Ended at :")
+                                  .setTimestamp();
+                                m.edit("** 🎉 GIVEAWAY ENDED 🎉**", {
+                                  embed: endEmbed
+                                });
+                              } else {
+                                let endEmbed = new Discord.RichEmbed()
+                                  .setColor()
+                                  .setTitle(title)
+                                  .setDescription(`Winners : ${gFilter}`)
+                                  .setFooter("Ended at :")
+                                  .setTimestamp();
+                                m.edit("** 🎉 GIVEAWAY ENDED 🎉**", {
+                                  embed: endEmbed
+                                });
+                              }
+                              if (gFilter === undefined) {
+                                // message.guild.channels.find("name" , room).send("No enough number of reactions")
+                              } else {
+                                message.guild.channels
+                                  .find("name", room)
+                                  .send(
+                                    `**Congratulations ${gFilter}! You won The \`${title}\`**`
+                                  );
+                              }
+                            }, ms(duration));
+                          });
+                      } catch (e) {
+                        message.channel.send(
+                          `:heavy_multiplication_x:| **i Don't Have Prem**`
+                        );
+                        console.log(e);
+                      }
+                    });
+                });
+              });
+          });
+        });
+    });
+  }
+});
+
+client.on("message", message => {
+  var prefix = ".";
+  if (!message.channel.guild) return;
+  if (message.content.startsWith(prefix + "move")) {
+    if (message.member.hasPermission("MOVE_MEMBERS")) {
+      if (message.mentions.users.size === 0) {
+        return message.channel.send("``Use : " + prefix + "move @User``");
+      }
+      if (message.member.voiceChannel != null) {
+        if (message.mentions.members.first().voiceChannel != null) {
+          var authorchannel = message.member.voiceChannelID;
+          var usermentioned = message.mentions.members.first().id;
+          var embed = new Discord.RichEmbed()
+            .setTitle("Succes!")
+            .setColor("#000000")
+            .setDescription(
+              `✅ You Have Moved <@${usermentioned}> To Your Channel `
+            );
+          var embed = new Discord.RichEmbed()
+            .setTitle(`You are Moved in ${message.guild.name} `)
+            .setColor("RANDOM")
+            .setTitle(`✽ **Premium**`)
+
+            .setDescription(
+              `**<@${message.author.id}> Moved You To His Channel!\nServer --> ${message.guild.name}**`
+            );
+          message.guild.members
+            .get(usermentioned)
+            .setVoiceChannel(authorchannel)
+            .then(m => message.channel.send(embed));
+          message.guild.members.get(usermentioned).send(embed);
+        } else {
+          message.channel.send(
+            "`You Cant Move" +
+              message.mentions.members.first() +
+              " `The User Should Be In channel To Move It`"
+          );
+        }
+      } else {
+        message.channel.send(
+          "**``You Should Be In Room Voice To Move SomeOne``**"
+        );
+      }
+    } else {
+      message.react("❌");
+    }
+  }
 });
 
 client.on("message", message => {
